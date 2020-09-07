b0ce2023bd04a1dbba4a72ead9f417868aa044ea
MelanX <info@melanx.de>
2020-09-07 19:08:26 +0200

    updated dependencies

--- src/main/java/de/melanx/MoreVanillaTools/util/data/DataCreator.java
+++ src/main/java/de/melanx/MoreVanillaTools/util/data/DataCreator.java
@@ -3,7 +3,7 @@ package de.melanx.MoreVanillaTools.util.data;
 import de.melanx.MoreVanillaTools.MoreVanillaTools;
 import net.minecraft.data.BlockTagsProvider;
 import net.minecraft.data.DataGenerator;
-import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
