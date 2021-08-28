f016f98d0b891b34ba74acfd1d0681130ace5598
MelanX <info@melanx.de>
2021-08-28 12:45:43 +0200

    re-add jei compat

--- src/main/java/de/melanx/MoreVanillaTools/util/JeiCompat.java
+++ src/main/java/de/melanx/MoreVanillaTools/util/JeiCompat.java
@@ -1,39 +1,38 @@
-// TODO re-add
-//package de.melanx.MoreVanillaTools.util;
-//
-//import de.melanx.morevanillalib.LibConfigHandler;
-//import mezz.jei.api.IModPlugin;
-//import mezz.jei.api.JeiPlugin;
-//import mezz.jei.api.registration.IRecipeRegistration;
-//import net.minecraft.resources.ResourceLocation;
-//
-//import javax.annotation.Nonnull;
-//
-//import static de.melanx.morevanillalib.compat.JeiCompat.PLUGIN_UID;
-//import static de.melanx.morevanillalib.compat.JeiCompat.addValueInfoPage;
-//
-//@JeiPlugin
-//public class JeiCompat implements IModPlugin {
-//
-//    @Nonnull
-//    @Override
-//    public ResourceLocation getPluginUid() {
-//        return PLUGIN_UID;
-//    }
-//
-//    @Override
-//    public void registerRecipes(@Nonnull IRecipeRegistration registration) {
-//        if (LibConfigHandler.doubleDrop.get()) {
-//            if (LibConfigHandler.coalDoubleDrop.get())
-//                addValueInfoPage(registration, Registry.COAL_PICKAXE.get(), "coal_double_drop", LibConfigHandler.coalDoubleDropChance.get() / 10);
-//            if (LibConfigHandler.emeraldDoubleDrop.get())
-//                addValueInfoPage(registration, Registry.EMERALD_PICKAXE.get(), "emerald_double_drop", LibConfigHandler.emeraldDoubleDropChance.get() / 10);
-//            if (LibConfigHandler.lapisDoubleDrop.get())
-//                addValueInfoPage(registration, Registry.LAPIS_PICKAXE.get(), "lapis_double_drop", LibConfigHandler.lapisDoubleDropChance.get() / 10);
-//            if (LibConfigHandler.quartzDoubleDrop.get())
-//                addValueInfoPage(registration, Registry.QUARTZ_PICKAXE.get(), "quartz_double_drop", LibConfigHandler.quartzDoubleDropChance.get() / 10);
-//            if (LibConfigHandler.redstoneDoubleDrop.get())
-//                addValueInfoPage(registration, Registry.REDSTONE_PICKAXE.get(), "redstone_double_drop", LibConfigHandler.redstoneDoubleDropChance.get() / 10);
-//        }
-//    }
-//}
+package de.melanx.MoreVanillaTools.util;
+
+import de.melanx.morevanillalib.config.FeatureConfig;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.resources.ResourceLocation;
+
+import javax.annotation.Nonnull;
+
+import static de.melanx.morevanillalib.compat.JeiCompat.PLUGIN_UID;
+import static de.melanx.morevanillalib.compat.JeiCompat.addValueInfoPage;
+
+@JeiPlugin
+public class JeiCompat implements IModPlugin {
+
+    @Nonnull
+    @Override
+    public ResourceLocation getPluginUid() {
+        return PLUGIN_UID;
+    }
+
+    @Override
+    public void registerRecipes(@Nonnull IRecipeRegistration registration) {
+        if (FeatureConfig.DoubleDrop.enabledAll) {
+            if (FeatureConfig.DoubleDrop.Coal.enabled)
+                addValueInfoPage(registration, ModItems.coalPickaxe, "coal_double_drop", FeatureConfig.DoubleDrop.Coal.chance * 100);
+            if (FeatureConfig.DoubleDrop.Emerald.enabled)
+                addValueInfoPage(registration, ModItems.emeraldPickaxe, "emerald_double_drop", FeatureConfig.DoubleDrop.Emerald.chance * 100);
+            if (FeatureConfig.DoubleDrop.Lapis.enabled)
+                addValueInfoPage(registration, ModItems.lapisPickaxe, "lapis_double_drop", FeatureConfig.DoubleDrop.Lapis.chance * 100);
+            if (FeatureConfig.DoubleDrop.Quartz.enabled)
+                addValueInfoPage(registration, ModItems.quartzPickaxe, "quartz_double_drop", FeatureConfig.DoubleDrop.Quartz.chance * 100);
+            if (FeatureConfig.DoubleDrop.Redstone.enabled)
+                addValueInfoPage(registration, ModItems.redstonePickaxe, "redstone_double_drop", FeatureConfig.DoubleDrop.Redstone.chance * 100);
+        }
+    }
+}
