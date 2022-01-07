4b42d6c638236e9cd43b86ef4e329c6066353b71
MelanX <info@melanx.de>
2022-01-07 16:00:28 +0100

    fix wrong item models
    close #31

--- src/main/java/de/melanx/MoreVanillaTools/data/ItemModelProvider.java
+++ src/main/java/de/melanx/MoreVanillaTools/data/ItemModelProvider.java
@@ -4,6 +4,8 @@ import io.github.noeppi_noeppi.libx.annotation.data.Datagen;
 import io.github.noeppi_noeppi.libx.data.provider.ItemModelProviderBase;
 import io.github.noeppi_noeppi.libx.mod.ModX;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.data.ExistingFileHelper;
 
 @Datagen
@@ -16,5 +18,10 @@ public class ItemModelProvider extends ItemModelProviderBase {
     @Override
     protected void setup() {
 
+    }
+
+    @Override
+    protected void defaultItem(ResourceLocation id, Item item) {
+        this.withExistingParent(id.getPath(), HANDHELD).texture("layer0", new ResourceLocation(id.getNamespace(), "item/" + id.getPath()));
     }
 }
